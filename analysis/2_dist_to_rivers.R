0d1ea6f2ad27ee7568d4e3b56073ef5a1b2a69fb
Guy Lomax <G.Lomax@exeter.ac.uk>
2024-09-18 13:17:17 +0100

    New script to calculate distance to nearest river

---
+++ analysis/2_dist_to_rivers.R
@@ -0,0 +1,39 @@
+## Distance from nearest river of specified stream order
+## 18th September 2024
+## Guy Lomax
+## G.Lomax@exeter.ac.uk
+
+library(terra)
+library(sf)
+library(dplyr)
+library(units)
+
+# Input parameters
+MIN_ORDER <- 4
+BUFFER_DIST <- 5 # km
+
+# Load data
+
+covariates <- rast("data/raw/raster/covariateMaps/staticVars.tif")
+
+river_files <- Sys.glob("data/raw/vector/Lin_rivers/*.shp")
+rivers <- lapply(river_files, st_read) %>%
+  bind_rows()
+
+# Filter to study area and to rivers greater than specified Strahler stream order
+
+study_area_buffered <- covariates %>%
+  st_bbox() %>%
+  st_as_sfc() %>%
+  st_buffer(dist = set_units(BUFFER_DIST, "km"))
+
+
+rivers_filtered <- rivers %>%
+  filter(strmOrder >= MIN_ORDER) %>%
+  st_filter(study_area_buffered)
+
+# Calculate distance to nearest river for each grid cell in study region
+
+rivers_rast <- rasterize(rivers_filtered, covariates, field = 1)
+
+dist_to_river <- distance(rivers_rast)
