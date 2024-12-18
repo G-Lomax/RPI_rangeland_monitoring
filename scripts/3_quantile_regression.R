4a29a1d56a1e378fe499a0f6c6b2ab5e0038c15a
G-Lomax <G.Lomax@exeter.ac.uk>
2024-12-18 12:03:44 +0000

    Increased sample size.
    Changed sample method to randomly sample cells and take all years, rather than randomly sampling all row-year combinations.

--- scripts/3_quantile_regression.R
+++ scripts/3_quantile_regression.R
@@ -10,21 +10,22 @@
 source("scripts/load.R")
 
 # Parallelisation
-nc <- availableCores() - 8
+nc <- availableCores() / 2
 plan(multicore, workers = nc)
 
 # Parameters
 
 YEARS <- 2000:2022
 MIN_STREAM_ORDER <- 4
-N_SAMPLE <- 10000
-SEED <- 451
+N_SAMPLE_POINTS <- 24000  # 1% of total sample region cells
+SEED <- 101
 
 ## 2. Load data ----
 
 static_covariates <- rast("data/raw/raster/covariate_maps/staticVars.tif")
 
 if(!file.exists("data/processed/raster/dynamic_covariates.tif")) {
+  message("Processing annual covariates")
   dynamic_covariates <- read_annual_rasters(YEARS, "data/raw/raster/covariate_maps") %>%
     mask_zeros(layer = paste0("GPP.", YEARS[1])) %>%
     terra::resample(
@@ -32,11 +33,13 @@ if(!file.exists("data/processed/raster/dynamic_covariates.tif")) {
       threads = TRUE, 
       filename = "data/processed/raster/dynamic_covariates.tif",
       overwrite = TRUE)  # Need to resample because Google Earth Engine...
+  message("Complete")
 } else {
+  message("Loading dynamic_covariates.tif")
   dynamic_covariates <- rast("data/processed/raster/dynamic_covariates.tif")
 }
 
-dist_to_river <- rast(paste0("data/processed/raster/dist_to_river/dist_to_river_", MIN_STREAM_ORDER, ".tif"))
+dist_to_river <- rast(paste0("data/processed/raster/dist_to_river_", MIN_STREAM_ORDER, ".tif"))
 
 names(dist_to_river) <- "dist_to_river"
 
@@ -45,17 +48,14 @@ twi <- rast("data/processed/raster/merit/merit_twi_fd8.tif")
 all_vars <- c(static_covariates, dist_to_river, twi, dynamic_covariates)
 
 ## 3. Generate random sample of training points ----
+
+var_df <- as.data.frame(all_vars, na.rm = TRUE, cells = TRUE, xy = TRUE)
+
 set.seed(SEED)
+cell_sample <- sample(unique(var_df$cell), N_SAMPLE_POINTS)
 
-var_sample <- spatSample(
-  x = all_vars,
-  size = N_SAMPLE,
-  na.rm = TRUE,
-  cells = TRUE,
-  xy = TRUE,
-  exp = 10)
-
-var_sample_long <- var_sample %>%
+var_sample_long <- var_df %>%
+  filter(cell %in% cell_sample) %>%
   pivot_longer(cols = contains(".")) %>%
   separate_wider_delim(name, delim = ".", names = c("var", "year")) %>%
   mutate(year = as.numeric(year)) %>%
@@ -79,7 +79,7 @@ var_sample_final <- var_sample_long %>%
 
 write_csv(var_sample_final, "data/processed/csv/var_sample_final.csv")
 
-## 5. Set up quantile regression model using mlr3 and ranger ----
+## 5. Set up quantile regression task using mlr3 ----
 
 # Create task
 
@@ -91,6 +91,7 @@ vars_to_retain <- c(
   
   # Annual vars
   "GPP",
+  "precipitation",
   "pptAnomaly",
   "pptIntensity",
   "pptMeanDayAnomaly",
@@ -116,6 +117,10 @@ var_sample_sf <- var_sample_subset %>%
   st_as_sf(crs = "EPSG:4326", coords = c("x", "y")) %>%
   st_transform("ESRI:54034")
 
+# # Use random subset of data as subset for initial feature selection
+# set.seed(SEED + 1)  # Set a different seed for subsampling
+# var_sample_sub <- slice_sample(var_sample_sf, prop = SUBSAMPLE_FRAC)
+
 task_gpp <- as_task_regr_st(
   x = var_sample_sf,
   id = "potential_gpp",
@@ -128,6 +133,20 @@ task_gpp$set_col_roles("year", roles = "time")
 
 write_rds(task_gpp, "data/processed/rds/regr_task_gpp.rds")
 
+# Create resampling strategy
+
+spt_cv_plan <- rsmp("sptcv_cstf", folds = 20)
+# sp_cv_plan <- rsmp("spcv_coords", folds = 20)
+# disc_cv_plan <- rsmp("spcv_disc", folds = 20, radius = 100000, buffer = 25000)
+
+## 6. Feature selection on subset of variables with initial tuning values ----
+
+# Define pipeline to take a random subsample of data for feature selection
+
+# set.seed(SEED + 1)
+# subset_po <- po("subsample", frac = SUBSAMPLE_FRAC)
+
+
 # Create learner (ranger random forest) with initial tuning values
 
 lrn_ranger_untuned <- lrn("regr.ranger", 
@@ -139,13 +158,7 @@ lrn_ranger_untuned <- lrn("regr.ranger",
                           respect.unordered.factors = "order"
 )
 
-# Create resampling strategy
-
-spt_cv_plan <- rsmp("sptcv_cstf", folds = 20)
-sp_cv_plan <- rsmp("spcv_coords", folds = 20)
-# disc_cv_plan <- rsmp("spcv_disc", folds = 20, radius = 100000, buffer = 25000)
-
-## 6. Feature selection with initial tuning values ----
+# lrn_ranger_sub <- as_learner(subset_po %>>% lrn_ranger_untuned)
 
 # Create forward feature selection with untuned model, minimising RMSE
 
@@ -163,13 +176,13 @@ spt_feature_select <- fsi(
   terminator = fs_term
 )
 
-sp_feature_select <- fsi(
-  task = task_gpp,
-  learner = lrn_ranger_untuned,
-  resampling = sp_cv_plan,
-  measure = perf_msr,
-  terminator = fs_term
-)
+# sp_feature_select <- fsi(
+#   task = task_gpp,
+#   learner = lrn_ranger_untuned,
+#   resampling = sp_cv_plan,
+#   measure = perf_msr,
+#   terminator = fs_term
+# )
 
 # Identify optimal feature set for each resampling strategy and store
 # Time ~ 10-12 hours
@@ -190,37 +203,37 @@ gc()
 pushoverr::pushover("Feature selection complete: Spatiotemporal CV")
 
 
-set.seed(456)
-tic()
-progressr::with_progress(
-  sp_feature_set <- fs_method$optimize(sp_feature_select)
-)
-toc()
-
-write_rds(sp_feature_select, "data/processed/rds/feature_selector_sp.rds")
-write_rds(sp_feature_set, "data/processed/rds/features_sp.rds")
-rm(sp_feature_select, sp_feature_set)
-gc()
-
-pushoverr::pushover("Feature selection complete: Spatial block CV")
+# set.seed(456)
+# tic()
+# progressr::with_progress(
+#   sp_feature_set <- fs_method$optimize(sp_feature_select)
+# )
+# toc()
+# 
+# write_rds(sp_feature_select, "data/processed/rds/feature_selector_sp.rds")
+# write_rds(sp_feature_set, "data/processed/rds/features_sp.rds")
+# rm(sp_feature_select, sp_feature_set)
+# gc()
+# 
+# pushoverr::pushover("Feature selection complete: Spatial block CV")
 
 
 ## 7. Hyperparameter tuning with final feature set ----
 
 # Load features to retain and assign to new tasks
 spt_feature_set <- read_rds("data/processed/rds/features_spt.rds")
-sp_feature_set <- read_rds("data/processed/rds/features_sp.rds")
+# sp_feature_set <- read_rds("data/processed/rds/features_sp.rds")
 
 task_gpp_spt <- task_gpp$clone()
 task_gpp_spt$select(unlist(spt_feature_set$features))
 
-task_gpp_sp <- task_gpp$clone()
-task_gpp_sp$select(unlist(sp_feature_set$features))
+# task_gpp_sp <- task_gpp$clone()
+# task_gpp_sp$select(unlist(sp_feature_set$features))
 
 # Define new quantile regression learners for (auto)tuning
 
 lrn_ranger_tuned_spt <- lrn(
-  "regr.ranger", 
+  "regr.ranger",
   predict_type = "response",
   quantreg = TRUE,
   keep.inbag = TRUE,
@@ -232,18 +245,18 @@ lrn_ranger_tuned_spt <- lrn(
   respect.unordered.factors = "order"
 )
 
-lrn_ranger_tuned_sp <- lrn(
-  "regr.ranger", 
-  predict_type = "response",
-  quantreg = TRUE,
-  keep.inbag = TRUE,
-  importance = "permutation",
-  num.trees = 1001,
-  mtry.ratio = to_tune(p_dbl(0, 1)),
-  min.node.size = to_tune(p_int(100, 1000)),
-  sample.fraction = to_tune(p_dbl(0.1, 0.9)),
-  respect.unordered.factors = "order"
-)
+# lrn_ranger_tuned_sp <- lrn(
+#   "regr.ranger", 
+#   predict_type = "response",
+#   quantreg = TRUE,
+#   keep.inbag = TRUE,
+#   importance = "permutation",
+#   num.trees = 1001,
+#   mtry.ratio = to_tune(p_dbl(0, 1)),
+#   min.node.size = to_tune(p_int(100, 1000)),
+#   sample.fraction = to_tune(p_dbl(0.1, 0.9)),
+#   respect.unordered.factors = "order"
+# )
 
 # Define auto-tuner objects for each model
 
@@ -259,13 +272,13 @@ at_spt <- auto_tuner(
   term_evals = 25
 )
 
-at_sp <- auto_tuner(
-  tuner = random_tuner,
-  learner = lrn_ranger_tuned_sp,
-  resampling = sp_cv_plan,
-  measure = perf_msr,
-  term_evals = 25
-)
+# at_sp <- auto_tuner(
+#   tuner = random_tuner,
+#   learner = lrn_ranger_tuned_sp,
+#   resampling = sp_cv_plan,
+#   measure = perf_msr,
+#   term_evals = 25
+# )
 
 # Tune and train models
 
@@ -284,18 +297,18 @@ gc()
 
 pushoverr::pushover("Tuning complete: Spatiotemporal CV model")
 
-# Spatial model
-
-set.seed(987)
-
-tic()
-rf_tuned_sp <-at_sp$train(task_gpp_sp)
-toc()
-
-write_rds(rf_tuned_sp, "data/processed/rds/rf_tuned_sp.rds")
-
-rm(rf_tuned_sp)
-gc()
-
-pushoverr::pushover("Tuning complete: Spatial block CV")
+# # Spatial model
+# 
+# set.seed(987)
+# 
+# tic()
+# rf_tuned_sp <-at_sp$train(task_gpp_sp)
+# toc()
+# 
+# write_rds(rf_tuned_sp, "data/processed/rds/rf_tuned_sp.rds")
+# 
+# rm(rf_tuned_sp)
+# gc()
+# 
+# pushoverr::pushover("Tuning complete: Spatial block CV")
 
