da115c751e7cc16bd7405d5728628c66fb6e2124
Guy Lomax <G.Lomax@exeter.ac.uk>
2024-12-31 12:25:06 +0000

    Updated fractional cover processing and model to include all fractional covers.

--- scripts/7A_Prosopis_mapping.R
+++ scripts/7A_Prosopis_mapping.R
@@ -22,27 +22,41 @@ lc_map <- rast("data/raw/raster/hunter2020/svmRadial_Multiple_season_time_series
 
 lc_points <- read_csv("data/raw/csv/hunter2020/training_data_acess.csv")
 
+lc_labels <- read_csv("data/raw/raster/hunter2020/hunter_raster_key.csv")
+
 ## 3. Prepare data ----
 
-lc_map_prosopis <- lc_map == 7
-
-lc_map_prosopis_reproj <- lc_map_prosopis %>%
+# Create rasters of fractional land cover at RPI resolution
+na_frac_map <- lc_map %>%
+  is.na() %>%
   project(rpi_map, method = "average") %>%
   trim()
+
+cover_frac_rasters <- map(1:9, function(x) {
+  binary_map <- lc_map == x
+  binary_map_reproj <- binary_map %>%
+    project(rpi_map, method = "average") %>%
+    trim() %>%
+    mask(na_frac_map <= 0.2, maskvalues = 0)
+}) %>% rast()
+
+names(cover_frac_rasters) <- lc_labels$label
+
+# RPI rasters
 
 rpi_only <- subset(rpi_map, str_detect(names(rpi_map), "rpi"))
 
 rpi_ts <- subset(rpi_only, str_detect(names(rpi_only), paste(START:END ,collapse = '|')))
 
 rpi_2018 <- subset(rpi_only, "rpi.2018") %>%
-  crop(lc_map_prosopis_reproj)
+  crop(cover_frac_rasters)
 
 rpi_mean <- rpi_ts %>%
-  crop(lc_map_prosopis_reproj) %>%
+  crop(cover_frac_rasters) %>%
   mean()
 
 rpi_trend <- rpi_ts %>%
-  crop(lc_map_prosopis_reproj) %>%
+  crop(cover_frac_rasters) %>%
   app(function(x) {
   if (any(is.na(x))) {
     NA
@@ -53,30 +67,41 @@ rpi_trend <- rpi_ts %>%
   }
 })
 
+names(rpi_2018) <- "rpi_2018"
+names(rpi_mean) <- "rpi_mean"
+names(rpi_trend) <- "rpi_trend"
 
-## 4. Assess RPI means and trends by Prosopis presence
 
-combined_rast <- c(lc_map_prosopis_reproj, rpi_2018, rpi_mean, rpi_trend)
-names(combined_rast) <- c("prosopis_frac", "rpi_2018", "rpi_mean", "rpi_trend")
+## 4. Assess RPI means and trends by Prosopis presence ----
 
-combined_df <- as.data.frame(combined_rast, na.rm = TRUE, cells = TRUE, xy = TRUE)
+cover_frac_combined <- c(cover_frac_rasters, rpi_2018, rpi_mean, rpi_trend)
+cover_frac_df <- as.data.frame(cover_frac_combined, na.rm = TRUE, cells = TRUE, xy = TRUE)
+
+# OLS multiple linear regression of RPI values against fractions of different
+# land covers (exclude the largest land cover (Grass Control) to avoid
+# collinearity - results will be relative to Grass Control mean value)
+
+cover_frac_rpi_2018 <- lm(
+  rpi_2018 ~ Acacia + `Cynodon plectostachyus` + `Ficus sur` + `General Control` +
+    `Mixed Vegetation Control` + Prosopis + `Sorghum bicolor` + `Sporobolus cordofanus`,
+  data = cover_frac_df
+)
+
+cover_frac_rpi_mean <- lm(
+  rpi_mean ~ Acacia + `Cynodon plectostachyus` + `Ficus sur` + `General Control` +
+    `Mixed Vegetation Control` + Prosopis + `Sorghum bicolor` + `Sporobolus cordofanus`,
+  data = cover_frac_df
+)
+
+cover_frac_rpi_trend <- lm(
+  rpi_trend ~ Acacia + `Cynodon plectostachyus` + `Ficus sur` + `General Control` +
+    `Mixed Vegetation Control` + Prosopis + `Sorghum bicolor` + `Sporobolus cordofanus`,
+  data = cover_frac_df
+)
 
 # Assess RPI means and trends of >80% pure pixels of different categories
 
-na_frac_map <- lc_map %>%
-  is.na() %>%
-  project(rpi_map, method = "average") %>%
-  crop(lc_map_prosopis_reproj)
 
-cover_frac_rasters <- map(1:9, function(x) {
-  binary_map <- lc_map == x
-  binary_map_reproj <- binary_map %>%
-    project(rpi_map, method = "average") %>%
-    trim()
-  
-  # Remove cells with > 20% NA
-  mask(binary_map_reproj, na_frac_map <= 0.2, maskvalues = 0)
-}) %>% rast()
 
 cover_pure <- cover_frac_rasters %>%
   mask(any(cover_frac_rasters >= 0.8), maskvalues = 0) %>%
