782bff4cec0b7806fc4ba98b1876b7610d3f8304
Guy Lomax <G.Lomax@exeter.ac.uk>
2025-02-08 16:19:31 +0000

    Removed spt model and cleaned up code.

--- scripts/3_quantile_regression.R
+++ scripts/3_quantile_regression.R
@@ -24,20 +24,15 @@ SEED <- 101
 
 static_covariates <- rast("data/raw/raster/covariate_maps/staticVars.tif")
 
-if(!file.exists("data/processed/raster/dynamic_covariates.tif")) {
-  message("Processing annual covariates")
-  dynamic_covariates <- read_annual_rasters(YEARS, "data/raw/raster/covariate_maps") %>%
-    mask_zeros(layer = paste0("GPP.", YEARS[1])) %>%
-    terra::resample(
-      static_covariates, 
-      threads = TRUE, 
-      filename = "data/processed/raster/dynamic_covariates.tif",
-      overwrite = TRUE)  # Need to resample because Google Earth Engine...
-  message("Complete")
-} else {
-  message("Loading dynamic_covariates.tif")
-  dynamic_covariates <- rast("data/processed/raster/dynamic_covariates.tif")
-}
+message("Processing annual covariates")
+dynamic_covariates <- read_annual_rasters(YEARS, "data/raw/raster/covariate_maps") %>%
+  mask_zeros(layer = paste0("GPP.", YEARS[1])) %>%
+  terra::resample(
+    static_covariates, 
+    threads = TRUE, 
+    filename = "data/processed/raster/dynamic_covariates.tif",
+    overwrite = TRUE)  # Need to resample because Google Earth Engine...
+message("Complete")
 
 dist_to_river <- rast(paste0("data/processed/raster/dist_to_river/dist_to_river_", MIN_STREAM_ORDER, ".tif"))
 
@@ -77,7 +72,7 @@ var_sample_final <- var_sample_long %>%
   ) %>%
   ungroup()
 
-write_csv(var_sample_final, "data/processed/csv/var_sample_final.csv")
+write_csv(var_sample_final, "data/processed/csv/var_sample_final2.csv")
 
 ## 5. Set up quantile regression task using mlr3 ----
 
@@ -96,7 +91,7 @@ vars_to_retain <- c(
   "pptIntensity",
   "pptMeanDayAnomaly",
   "ugi",
-  "temperature_2m_mean",
+  "temperature_2m",
   "potential_evaporation_sum",
   "GMT_0900_PAR",
   
@@ -117,10 +112,6 @@ var_sample_sf <- var_sample_subset %>%
   st_as_sf(crs = "EPSG:4326", coords = c("x", "y")) %>%
   st_transform("ESRI:54034")
 
-# # Use random subset of data as subset for initial feature selection
-# set.seed(SEED + 1)  # Set a different seed for subsampling
-# var_sample_sub <- slice_sample(var_sample_sf, prop = SUBSAMPLE_FRAC)
-
 task_gpp <- as_task_regr_st(
   x = var_sample_sf,
   id = "potential_gpp",
@@ -136,7 +127,6 @@ write_rds(task_gpp, "data/processed/rds/regr_task_gpp.rds")
 # Create resampling strategy
 
 sp_cv_plan <- rsmp("spcv_coords", folds = 20)
-spt_cv_plan <- rsmp("sptcv_cstf", folds = 20)
 
 ## 6. Feature selection on subset of variables with initial tuning values ----
 
@@ -150,8 +140,6 @@ lrn_ranger_untuned <- lrn("regr.ranger",
                           sample.fraction = 0.5,
                           respect.unordered.factors = "order"
 )
-
-# lrn_ranger_sub <- as_learner(subset_po %>>% lrn_ranger_untuned)
 
 # Create forward feature selection with untuned model, minimising RMSE
 
@@ -169,18 +157,9 @@ sp_feature_select <- fsi(
   terminator = fs_term
 )
 
-spt_feature_select <- fsi(
-  task = task_gpp,
-  learner = lrn_ranger_untuned,
-  resampling = spt_cv_plan,
-  measures = perf_msr,
-  terminator = fs_term
-)
-
 # Identify optimal feature set for each resampling strategy and store
-# Time ~ 10-12 hours
+# Time ~ 2-3 days
 
-# Spatial
 set.seed(456)
 tic()
 progressr::with_progress(
@@ -195,39 +174,20 @@ gc()
 
 pushoverr::pushover("Feature selection complete: Spatial block CV")
 
-# Spatiotemporal
-set.seed(123)
-
-tic()
-progressr::with_progress(
-  spt_feature_set <- fs_method$optimize(spt_feature_select)
-)
-toc()
-
-
-write_rds(spt_feature_select, "data/processed/rds/feature_selector_spt.rds")
-write_rds(spt_feature_set, "data/processed/rds/features_spt.rds")
-rm(spt_feature_select, spt_feature_set)
-gc()
-pushoverr::pushover("Feature selection complete: Spatiotemporal CV")
-
 ## 7. Hyperparameter tuning with final feature set ----
 
-# Load features to retain and assign to new tasks
+# Load features to retain and assign to new task
 sp_feature_set <- read_rds("data/processed/rds/features_sp.rds")
-spt_feature_set <- read_rds("data/processed/rds/features_spt.rds")
 
 task_gpp_sp <- task_gpp$clone()
 task_gpp_sp$select(unlist(sp_feature_set$features))
 
-task_gpp_spt <- task_gpp$clone()
-task_gpp_spt$select(unlist(spt_feature_set$features))
-
-# Define new quantile regression learners for (auto)tuning
+# Define new quantile regression learner for (auto)tuning
 
 lrn_ranger_tuned_sp <- lrn(
   "regr.ranger",
   predict_type = "response",
+  # quantiles = c(0.5, 0.9),
   quantreg = TRUE,
   keep.inbag = TRUE,
   importance = "permutation",
@@ -238,21 +198,7 @@ lrn_ranger_tuned_sp <- lrn(
   respect.unordered.factors = "order"
 )
 
-lrn_ranger_tuned_spt <- lrn(
-  "regr.ranger",
-  predict_type = "response",
-  quantreg = TRUE,
-  keep.inbag = TRUE,
-  importance = "permutation",
-  num.trees = 1001,
-  mtry.ratio = to_tune(p_dbl(0, 1)),
-  min.node.size = to_tune(p_int(100, 1000)),
-  sample.fraction = to_tune(p_dbl(0.1, 0.9)),
-  respect.unordered.factors = "order"
-)
-
-
-# Define auto-tuner objects for each model
+# Define auto-tuner object
 
 random_tuner <- tnr("random_search")
 
@@ -262,14 +208,6 @@ at_sp <- auto_tuner(
   tuner = random_tuner,
   learner = lrn_ranger_tuned_sp,
   resampling = sp_cv_plan,
-  measure = perf_msr,
-  term_evals = 25
-)
-
-at_spt <- auto_tuner(
-  tuner = random_tuner,
-  learner = lrn_ranger_tuned_spt,
-  resampling = spt_cv_plan,
   measure = perf_msr,
   term_evals = 25
 )
@@ -284,27 +222,10 @@ tic()
 rf_tuned_sp <-at_sp$train(task_gpp_sp)
 toc()
 
-write_rds(rf_tuned_sp, "data/processed/rds/rf_tuned_sp.rds")
+write_rds(rf_tuned_sp, "data/processed/rds/rf_tuned_sp2.rds")
 
 rm(rf_tuned_sp)
 gc()
 
 pushoverr::pushover("Tuning complete: Spatial block CV")
-
-# Spatiotemporal model
-
-set.seed(789)
-
-tic()
-rf_tuned_spt <- at_spt$train(task_gpp_spt)
-toc()
-
-write_rds(rf_tuned_spt, "data/processed/rds/rf_tuned_spt.rds")
-
-rm(rf_tuned_spt)
-gc()
-
-pushoverr::pushover("Tuning complete: Spatiotemporal CV model")
-
-
 
